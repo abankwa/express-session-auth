5369762cb95624788957b620a24237fe8ef6f6ba
Derrick Abankwa <dkabankwa@yahoo.com>
2020-07-25 01:37:07 -0700

    update

--- views/Logout.jsx
+++ views/Logout.jsx
@@ -3,22 +3,24 @@ const BaseLayout = require('./BaseLayout')
 
 function Logout(props){
     return (
-        <div className="container">
-        <div className="nav">
-            <h1>Logout</h1>
-            <ul>
-                <a href="/"><li>Home</li></a>
-            </ul>
+        <BaseLayout>
+            <div className="container">
+            <div className="nav">
+                <h1>Logout</h1>
+                <ul>
+                    <a href="/"><li>Home</li></a>
+                </ul>
+            </div>
+
+            <div className="showcase"></div>
+
+            <div className="dashboard">
+                <p>Your session has expired</p>
+                <a href="/">Login</a> or <a href="/signup">Sign Up</a>
+            </div>
+
         </div>
-
-        <div className="showcase"></div>
-
-        <div className="dashboard">
-            <p>Your session has expired</p>
-            <a href="/">Login</a> or <a href="/signup">Sign Up</a>
-        </div>
-
-    </div>
+    </BaseLayout>
     )
 }
 
